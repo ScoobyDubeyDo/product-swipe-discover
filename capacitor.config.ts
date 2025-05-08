727cac5d76436c9912bf8944385b9256760c1cfa
Aman Dubey <aman.dubey1998@gmail.com>
2025-05-08 22:21:25 +0530

    feat :sparkles: - Adds capacitor for android app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'product-swipe',
+  webDir: 'dist'
+};
+
+export default config;
